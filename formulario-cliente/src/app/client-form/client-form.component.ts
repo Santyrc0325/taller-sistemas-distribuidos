9a98f724c3f2193654ed29ce6dec710582939ce4
Andres Felipe Nieto Jimenez <anieto@uniempresarial.edu.co>
2024-08-03 22:09:31 -0500

    Forms with validations

--- formulario-cliente/src/app/client-form/client-form.component.ts
+++ formulario-cliente/src/app/client-form/client-form.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-client-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './client-form.component.html',
   styleUrl: './client-form.component.css'
 })
 export class ClientFormComponent {
+  clientForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.clientForm = this.fb.group({
+      nombres: ['', [Validators.required]],
+      apellidos: ['', [Validators.required]],
+      celular: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      nombreUsuario: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.clientForm.valid) {
+      if (this.clientForm.value.password !== this.clientForm.value.confirmPassword) {
+        alert('Las contraseñas no coinciden');
+      } else {
+        // Lógica para enviar el formulario
+      }
+    } else{
+      this.clientForm.markAllAsTouched();
+    }
+  }
 }
